5a42fb40001bcb80757ecbf3e497708494a5ee5d
Vlad <vladyslav.braslavskyi@flip.shop>
2024-11-17 16:15:18 +0100

    feat: integrate validator and transformer

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
@@ -7,6 +8,9 @@ import { appOptions } from './app.options';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   await app.listen(+configService.get(appOptions.port));
 }
 bootstrap();
